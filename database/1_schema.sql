4695a5a2b5e5ef30f484c40b0ba65682d57bbf6c
ayumin <58648147+shoumoji@users.noreply.github.com>
2022-10-01 20:03:12 +0900

    fix: POINT型は緯度経度両方のデータを持っているため、変更
    Signed-off-by: ayumin <58648147+shoumoji@users.noreply.github.com>

--- database/1_schema.sql
+++ database/1_schema.sql
@@ -1,8 +1,7 @@
 CREATE TABLE `countries` (
   `code` CHAR(2) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `latitude` POINT NOT NULL,
-  `longitude` POINT NOT NULL,
+  `location` POINT NOT NULL,
   `population` INT UNSIGNED,
   PRIMARY KEY (`code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
